8b1b1f70a35f0a5ff9dcdf0d557a06c94f1da3bc
royfalk <roy@falk.co.il>
2015-02-16 23:11:06 +0200

    Further improvements to starMapView - Begin work on zoom and gestures

--- app/src/main/java/il/co/falk/andromeda/StarMapView.java
+++ app/src/main/java/il/co/falk/andromeda/StarMapView.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -22,8 +26,13 @@ import il.co.falk.andromeda.game.Universe;
 /**
  * Created by roy on 1/21/15.
  */
-public class StarMapView extends View  {
+public class StarMapView extends View implements
+        GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener  {
     Paint paint;
+    private GestureDetectorCompat mDetector;
+    private static final String DEBUG_TAG = "star map gesture";
+    private int x,y,zoom;
 
     public StarMapView(Context context) {
         super(context);
@@ -43,6 +52,13 @@ public class StarMapView extends View  {
     private void init(Context context) {
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setColor(Color.GREEN);
+
+        mDetector = new GestureDetectorCompat(context,this);
+        mDetector.setOnDoubleTapListener(this);
+
+        x = 0;
+        y = 0;
+        zoom = 1;
     }
 
 
@@ -105,6 +121,79 @@ public class StarMapView extends View  {
             canvas.drawCircle(x,y,5, paint);
         }
     }
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+    // Gestures
+    ////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        this.mDetector.onTouchEvent(event);
+        // Be sure to call the superclass implementation
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent event) {
+        Log.d(DEBUG_TAG,"onDown: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2,
+                           float velocityX, float velocityY) {
+        Log.d(DEBUG_TAG, "onFling: " + event1.toString()+event2.toString());
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onLongPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+                            float distanceY) {
+        Log.d(DEBUG_TAG, "onScroll: " + e1.toString()+e2.toString());
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onShowPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onSingleTapUp: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDoubleTap: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDoubleTapEvent: " + event.toString());
+
+        if(zoom>1) zoom/=2;
+        Log.d("zoom", String.valueOf(zoom));
+
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onSingleTapConfirmed: " + event.toString());
+
+        if(zoom<16) zoom*=2;
+        Log.d("zoom", String.valueOf(zoom));
+
+        return true;
+    }
+
 }
 
 
